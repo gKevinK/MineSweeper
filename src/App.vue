29a3b8998cce8501028af8489e7c9307c819b914
KevinK <gkangkai@gmail.com>
2019-01-09 22:36:42 +0800

    ...

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,34 @@
 <template>
-  <div id="app">
-    <div>Hello world!</div>
-    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
-  </div>
+    <div id="app" >
+        <div>Hello world!</div>
+        <Minefield style="position: absolute; top: 20px; bottom: 20px; left: 20px; right:20px; "
+                   ref="ui" :board="field"/>
+    </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-// import HelloWorld from './components/HelloWorld.vue';
+import Minefield from './Minefield.vue';
 
 @Component({
-  components: {
-    // HelloWorld,
-  },
+    components: {
+        Minefield,
+    },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+
+    field: Array<Array<string>> = [['x', 'x'], ['x', 'y']];
+
+}
 </script>
 
 <style scoped>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    height: 100%;
 }
 </style>
---
+++ src/Minefield.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+    <div id="field">Minefield</div>
+    <div v-for="(row, x) in board" :key="row.id">
+        <div v-for="(cell, y) in row" :key="cell.id" @click="onClick(x, y)">
+            {{ cell }}
+        </div>
+    </div>
+</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class MineSweeper extends Vue {
+    @Prop({ default: [] }) board!: Array<Array<string>>;
+
+    onClick (x: number, y: number) {
+        
+    }
+}
+</script>
+
+<style scoped>
+#field {
+    background: gray;
+}
+</style>
